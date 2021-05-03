82cd02019615d49df35c7a90eaa44156912161c0
SamSwaroop <samswaroop97@msitprogram.net>
2021-05-03 22:31:38 +0530

    basic UI[beta 1.0]

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Grid from '@material-ui/core/Grid';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,23 +21,81 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  pos:{
+    textAlign:'center',
+  },
 }));
 
 
 function App() {
   const classes = useStyles();
+  <Grid
+  container
+  spacing={0}
+  direction="row"
+  alignItems="center"
+  justify="center"
+  display="flex"
+  style={{ minHeight: '100vh' }}
+ />
   return (
-    <AppBar position="static">
+    <div>
+      <div>
+    <AppBar position="fixed">
   <Toolbar>
-    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+    {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
       <MenuIcon />
-    </IconButton>
+    </IconButton> */}
     <Typography variant="h6" className={classes.title}>
-      News
+      Exam App
     </Typography>
     <Button color="inherit">Login</Button>
+    <Button color="inherit">SignUp</Button>
   </Toolbar>
 </AppBar>
+</div>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+<br></br>
+
+<div className={classes.root}>
+
+
+  
+<Card
+        style={{
+          width: 1000,
+          margin:"auto",
+          
+          backgroundColor: "light grey",
+        }}
+      >
+<CardContent>
+          
+          <Typography variant="body2" component="p" className={classes.pos}>
+            <h3 >Instructions:</h3>
+          </Typography>
+          <Typography variant="body1" component="p">
+            <p>Dear Candidate<br></br>1.You are taking this exam from your residence or any convinient location.<br></br>2. You are required to ensure that the following conditions are adhered to before sitting for the test</p>
+          </Typography>
+          <Typography variant="body1" component="p">
+
+            
+
+          </Typography>
+        </CardContent>
+        </Card>
+    </div>
+</div>
+
+
+  
+        
+     
   );
 }
 
