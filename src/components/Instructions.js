0c241235198f29da6bff03620b76b261e26ca3aa
vamsee9 <4n0n5ur7@protonmail.com>
2021-05-04 22:32:08 +0530

    [deta 1.2] [Form 1/2] added login page
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

---
+++ src/components/Instructions.js
@@ -0,0 +1,155 @@
+import React from 'react';
+import { useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  pos: {
+    textAlign: "center",
+  },
+}));
+
+
+export default function Instructions() {
+
+    const history = useHistory();
+
+    const routeChange = () => {
+      let path = "/Login";
+      history.push(path);
+    };
+
+    const classes = useStyles();
+    <Grid
+      container
+      spacing={0}
+      direction="row"
+      alignItems="center"
+      justify="center"
+      display="flex"
+      style={{ minHeight: "100vh" }}
+    />;
+    return (
+      <div>
+        <div>
+          <AppBar position="fixed">
+            <Toolbar>
+              {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <MenuIcon />
+    </IconButton> */}
+
+              <Typography variant="h6" className={classes.title}>
+                Exam App
+              </Typography>
+              {/* <Button color="inherit">Login</Button>
+    <Button color="inherit">SignUp</Button> */}
+            </Toolbar>
+          </AppBar>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <div className={classes.root}>
+          <Card
+            style={{
+              width: 1000,
+              margin: "auto",
+              backgroundColor: "",
+            }}
+          >
+            <CardContent>
+              <Typography variant="body2" component="p" className={classes.pos}>
+                <h1>Instructions</h1>
+              </Typography>
+              <Typography variant="body1" component="p">
+                <b>Dear Candidate</b>
+                <ol>
+                  <li>
+                    You are taking this exam from your residence or any
+                    convenient location
+                  </li>
+                  <li>
+                    You are required to ensure that the following conditions are
+                    adhered to before sitting for the test
+                  </li>
+                </ol>
+
+                <h3>Do's and Don'ts</h3>
+
+                <b>Do's:</b>
+                <ol>
+                  <li>
+                    Ensure that your computer has the following specifications
+                  </li>
+                  <ul>
+                    <li>Computer-Pentium IV/Dual core,1GB RAM or higher,</li>
+                    <li>80GB HDD or higher</li>
+                    <li>15' colour Monitor,keyboard and mouse</li>
+                    <li>Microsoft Windows 10 and Antivirus</li>
+                    <li>Webcam and microphone</li>
+                    <li>Internet connection of at least 1MBPS</li>
+                  </ul>
+                  <li>
+                    Place the computer so that your back is facing the wall and
+                    there are no devices placed in the background
+                  </li>
+                  <li>
+                    The room where you sit should be isolated and only you need
+                    to be present for complete exam duration
+                  </li>
+                  <li>
+                    Sit at table with computer.Keep the desk clear of
+                    objects,papers books and any other electronic material like
+                    mobiles
+                  </li>
+                  <li>
+                    Ensure proper lighting so that your face is clearly visible
+                    to the camera
+                  </li>
+                  <li>Ensure your dressed formally</li>
+                </ol>
+
+                <b>Don'ts:</b>
+
+                <ol>
+                  <li>Do not take help from others</li>
+                  <li>Do not wear any bluetooth devices</li>
+                  <li>Do not read out anything loudly</li>
+                  <li>Do not leave the work station while taking the test</li>
+                  <li>
+                    Do not let anyone in the room and avoid background noise
+                  </li>
+                </ol>
+                <CardContent className={classes.pos}>
+                  <Button variant="contained" onClick={routeChange}>
+                    Get Started
+                  </Button>
+                </CardContent>
+              </Typography>
+              <Typography variant="body1" component="p"></Typography>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    );
+
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,39 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  pos: {
+    textAlign: "center",
+  },
+}));
+
+export default function Nav() {
+    const classes = useStyles();
+    return (
+      <AppBar position="fixed">
+        <Toolbar>
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <MenuIcon />
+    </IconButton> */}
+
+          <Typography variant="h6" className={classes.title}>
+            Exam App
+          </Typography>
+          {/* <Button color="inherit">Login</Button>
+    <Button color="inherit">SignUp</Button> */}
+        </Toolbar>
+      </AppBar>
+    );
+}
