7aca72f2e4d6a17e5c6325b885edb697446e89eb
vamsee9 <4n0n5ur7@protonmail.com>
2021-05-05 10:22:52 +0530

    code clean up
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import Grid from "@material-ui/core/Grid";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((_theme) => ({
   root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
     flexGrow: 1,
   },
   pos: {
@@ -37,37 +26,8 @@ export default function Instructions() {
     };
 
     const classes = useStyles();
-    <Grid
-      container
-      spacing={0}
-      direction="row"
-      alignItems="center"
-      justify="center"
-      display="flex"
-      style={{ minHeight: "100vh" }}
-    />;
     return (
       <div>
-        <div>
-          <AppBar position="fixed">
-            <Toolbar>
-              {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-      <MenuIcon />
-    </IconButton> */}
-
-              <Typography variant="h6" className={classes.title}>
-                Exam App
-              </Typography>
-              {/* <Button color="inherit">Login</Button>
-    <Button color="inherit">SignUp</Button> */}
-            </Toolbar>
-          </AppBar>
-        </div>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-
         <div className={classes.root}>
           <Card
             style={{
