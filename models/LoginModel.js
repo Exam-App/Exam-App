9aac145746a794cb09805f770b8d578de262be6c
vamsee9 <4n0n5ur7@protonmail.com>
2021-05-05 22:54:31 +0530

    [Backend] Connected to database
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

---
+++ models/LoginModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const loginTemplate = new mongoose.Schema({
+    username: {
+        type: String,
+        required:true
+    },
+    password: {
+        type: String,
+        required:true
+    },
+    date: {             // this adds student login timestamp in backend
+        type: Date,
+        default:Date.now
+    }
+})
+
+// student is database table name
+module.exports = mongoose('student', loginTemplate)
