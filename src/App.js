a00ccf62e5167a252d14095c86b11eab20a2ae26
Vemula Saikiran <71496561+saikiran534@users.noreply.github.com>
2021-05-07 20:31:31 +0530

    Faculty Loging Page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import LoginTab from "./components/Login"
-
+import FacultyLoginTab from"./components/FacultyLoin"
 import SignUpTab from "./components/SignUp"
 import Instructions from "./components/Instructions";
 
@@ -17,6 +17,7 @@ class App extends React.Component {
         <Route path="/login" component={LoginTab} />
         
         <Route path="/signup" component={SignUpTab} />
+        <Route path="/faculty" component={FacultyLoginTab} />
       </div>
     </Router>
   );
