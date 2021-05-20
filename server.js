f9dd8179c7acb5cb9110fed016a68058379940a6
vamsee9 <4n0n5ur7@protonmail.com>
2021-05-20 10:29:17 +0530

    [checkpoint - 1] added cookies
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

--- server.js
+++ server.js
@@ -1,27 +1,53 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const facultyUrls = require('./router/facultyRoute')
-const studentUrls = require("./router/studentRoute")
-const cors=require('cors')
-dotenv.config()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const facultyUrls = require("./router/facultyRoute");
+const studentUrls = require("./router/studentRoute");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+dotenv.config();
 
-mongoose.connect(process.env.DATABASE_ACCESS, {
-    useNewUrlParser : true,
-    useUnifiedTopology : true,
-}, () => console.log("Database Connected..") )
+mongoose.connect(
+  process.env.DATABASE_ACCESS,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log("Database Connected..")
+);
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-    ],
+    origin: ["http://localhost:3000"],
     credentials: true,
   })
 );
-app.use('/app', facultyUrls)
-app.use('/app', studentUrls)
+app.use("/app", facultyUrls);
+app.use("/app", studentUrls);
 
-app.listen(4000, () => console.log("server is up and running"))
+app.listen(4000, () => console.log("server is up and running"));
+
+// cookies
+app.get("/set-cookies", (request, response) => {
+  //response.setHeader("Set-Cookie", "newStudent=true");
+
+  response.cookie("newStudent", false);
+  response.cookie("isFaculty", true, {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+  });
+
+  response.send("you got the cookies!");
+});
+
+app.get("/read-cookies", (request, response) => {
+
+  const cookies = request.cookies;
+  console.log(cookies.newStudent);
+
+  response.json(cookies)
+
+});
