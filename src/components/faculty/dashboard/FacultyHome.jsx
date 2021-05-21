b2f7fb0b31de54a59ffef4bdbde1165dc4c64e68
vamsee9 <4n0n5ur7@protonmail.com>
2021-05-21 18:04:11 +0530

    code clean up
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

--- src/components/faculty/dashboard/FacultyHome.jsx
+++ src/components/faculty/dashboard/FacultyHome.jsx
@@ -2,7 +2,7 @@ import { Box, makeStyles } from "@material-ui/core";
 import React, { useState } from "react";
 import * as XLSX from "xlsx";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   // Load app bar information from the theme
 }));
 
@@ -27,7 +27,6 @@ export default function FacultyHome() {
 
     const readFile=()=> {
     var f = state.file;
-    var name = f.name;
     const reader = new FileReader();
     reader.onload = (evt) => {
       // evt = on_file_select event
