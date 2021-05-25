e6ceaab45faf96476a29dfe6aea73ffd4d169cc1
SamSwaroop <samswaroop97@msitprogram.net>
2021-05-25 23:36:21 +0530

    Conversion from Xcel to Json and Displaying JSON quiz

--- src/components/faculty/dashboard/FacultyHome.jsx
+++ src/components/faculty/dashboard/FacultyHome.jsx
@@ -1,95 +1,165 @@
-import { Box, makeStyles } from "@material-ui/core";
+// import { Box, makeStyles } from "@material-ui/core";
+// import React, { useState } from "react";
+// import * as XLSX from "xlsx";
+
+// const useStyles = makeStyles(() => ({
+//   // Load app bar information from the theme
+// }));
+
+// export default function FacultyHome() {
+//   const classes = useStyles();
+//   const [state,setState]=useState({
+//     file: "",
+//   })
+
+
+//   const filePathset=(e) =>{
+//     e.stopPropagation();
+//     e.preventDefault();
+//     var file = e.target.files[0];
+//     console.log(file);
+//     setState({ file });
+
+//     console.log(state.file);
+//   }
+
+
+
+//     const readFile=()=> {
+//     var f = state.file;
+//     const reader = new FileReader();
+//     reader.onload = (evt) => {
+//       // evt = on_file_select event
+//       /* Parse data */
+//       const bstr = evt.target.result;
+//       const wb = XLSX.read(bstr, { type: "binary" });
+//       /* Get first worksheet */
+//       const wsname = wb.SheetNames[0];
+//       const ws = wb.Sheets[wsname];
+//       /* Convert array of arrays */
+//       const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+//       /* Update state */
+//       console.log("Data>>>" + data);// shows that excel data is read
+//       console.log(convertToJson(data)); // shows data in json format
+//     };
+//     reader.readAsBinaryString(f);
+//   }
+
+
+
+
+
+//     const convertToJson=(csv)=> {
+//     var lines = csv.split("\n");
+
+//     var result = [];
+
+//     var headers = lines[0].split(",");
+
+//     for (var i = 1; i < lines.length; i++) {
+//       var obj = {};
+//       var currentline = lines[i].split(",");
+
+//       for (var j = 0; j < headers.length; j++) {
+//         obj[headers[j]] = currentline[j];
+//       }
+
+//       result.push(obj);
+//     }
+
+//     //return result; //JavaScript object
+//     return JSON.stringify(result); //JSON
+//   }
+
+
+//   return (
+//     <Box className={classes.toolbar} width="45%" height="10%">
+//       <div>
+//         <input
+//           type="file"
+//           id="file"
+          
+//           onChange={filePathset}
+//         />
+//         <button
+//           onClick={() => {
+//             readFile();
+//           }}
+//         >
+//           Read File
+//         </button>
+//       </div>
+      
+//     </Box>
+//   );
+// }
+
 import React, { useState } from "react";
 import * as XLSX from "xlsx";
 
-const useStyles = makeStyles(() => ({
-  // Load app bar information from the theme
-}));
+function FacultyHome() {
+  const [items, setItems] = useState([]);
 
-export default function FacultyHome() {
-  const classes = useStyles();
-  const [state,setState]=useState({
-    file: "",
-  })
+  const readExcel = (file) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
 
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
 
-  const filePathset=(e) =>{
-    e.stopPropagation();
-    e.preventDefault();
-    var file = e.target.files[0];
-    console.log(file);
-    setState({ file });
+        const wb = XLSX.read(bufferArray, { type: "buffer" });
 
-    console.log(state.file);
-  }
+        const wsname = wb.SheetNames[0];
 
+        const ws = wb.Sheets[wsname];
 
+        const data = XLSX.utils.sheet_to_json(ws);
 
-    const readFile=()=> {
-    var f = state.file;
-    const reader = new FileReader();
-    reader.onload = (evt) => {
-      // evt = on_file_select event
-      /* Parse data */
-      const bstr = evt.target.result;
-      const wb = XLSX.read(bstr, { type: "binary" });
-      /* Get first worksheet */
-      const wsname = wb.SheetNames[0];
-      const ws = wb.Sheets[wsname];
-      /* Convert array of arrays */
-      const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
-      /* Update state */
-      console.log("Data>>>" + data);// shows that excel data is read
-      console.log(convertToJson(data)); // shows data in json format
-    };
-    reader.readAsBinaryString(f);
-  }
+        resolve(data);
+      };
 
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
 
-
-
-
-    const convertToJson=(csv)=> {
-    var lines = csv.split("\n");
-
-    var result = [];
-
-    var headers = lines[0].split(",");
-
-    for (var i = 1; i < lines.length; i++) {
-      var obj = {};
-      var currentline = lines[i].split(",");
-
-      for (var j = 0; j < headers.length; j++) {
-        obj[headers[j]] = currentline[j];
-      }
-
-      result.push(obj);
-    }
-
-    //return result; //JavaScript object
-    return JSON.stringify(result); //JSON
-  }
-
+    promise.then((d) => {
+      setItems(d);
+    });
+  };
 
   return (
-    <Box className={classes.toolbar} width="45%" height="10%">
-      <div>
-        <input
-          type="file"
-          id="file"
-          
-          onChange={filePathset}
-        />
-        <button
-          onClick={() => {
-            readFile();
-          }}
-        >
-          Read File
-        </button>
-      </div>
+    <div>
+      <input
+        type="file"
+        onChange={(e) => {
+          const file = e.target.files[0];
+          readExcel(file);
+        }}
+      />
+
       
-    </Box>
+          {items.map((d) => (
+            <>
+            
+              <p>{d.Question}</p>
+              <input type="radio" id="male" name="gender" value="male" />
+              <label for="male">{d.A}</label><br></br>
+              <input type="radio" id="male" name="gender" value="male" />
+              <label for="male">{d.B}</label><br></br>
+              <input type="radio" id="male" name="gender" value="male" />
+              <label for="male">{d.C}</label><br></br>
+              <input type="radio" id="male" name="gender" value="male" />
+              <label for="male">{d.D}</label><br></br>
+              <input type="radio" id="male" name="gender" value="male" />
+              <label for="male">{d.E}</label><br></br>
+              
+            </>
+          ))}
+        
+    </div>
   );
 }
+
+export default FacultyHome;
