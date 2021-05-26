c7aff12387048efec1b20c7a20d4832df7a338f4
vamsee9 <4n0n5ur7@protonmail.com>
2021-05-26 11:23:37 +0530

    display session message
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -1,3 +1,4 @@
+import { Typography } from "@material-ui/core";
 import axios from "axios";
 import React, { createContext, useEffect, useState } from "react";
 
@@ -5,21 +6,37 @@ const AuthContext = createContext();
 
 function AuthContextProvider(props) {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [state, setState] = useState({
+    message: "",
+  });
 
   async function getLoggedIn() {
     const loggedInRes = await axios.get("http://localhost:4000/app/loggedIn");
+    if (loggedInRes.data === false) {
+      setState({ message: "Unauthorized" });
+    } else if (loggedInRes.data === true) {
+      setState({message: "Session in Progress can't go back to Login or Registration."})
+    }
     setLoggedIn(loggedInRes.data);
   }
 
+  // if (!getLoggedIn) return <h1>Unauthorized</h1>;
   useEffect(() => {
     getLoggedIn();
   }, []);
 
   return (
-    <AuthContext.Provider
-      value={{ loggedIn, getLoggedIn }}
+    <AuthContext.Provider value={{ loggedIn, getLoggedIn }}>
+      {props.children}
+      <Typography
+        variant="h3"
+        component="p"
+        align="center"
+        color="textPrimary"
+        gutterBottom
       >
-          {props.children}
+        {state.message}
+      </Typography>
     </AuthContext.Provider>
   );
 }
