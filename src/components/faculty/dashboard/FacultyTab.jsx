551530f26dca9710e778409266e21b521e38db4a
vamsee9 <4n0n5ur7@protonmail.com>
2021-05-29 18:33:44 +0530

    remove unused callbacks
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

--- src/components/faculty/dashboard/FacultyTab.jsx
+++ src/components/faculty/dashboard/FacultyTab.jsx
@@ -1,16 +1,8 @@
-import { Box, makeStyles } from "@material-ui/core";
+import { Box } from "@material-ui/core";
 import Registration from "../../student/Registration";
 import React from "react";
 
-const useStyles = makeStyles((theme) => ({
-  // Load app bar information from the theme
-}));
-
 export default function FacultyTab() {
-  const classes = useStyles();
-
-
-
   return (
     <Box width="40%">
       <div>
