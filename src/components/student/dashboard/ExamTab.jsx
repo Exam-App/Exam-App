f412d62aec3d10621547861ebc7042590b2476a3
vamsee9 <4n0n5ur7@protonmail.com>
2021-06-01 18:00:13 +0530

    checkPoint 3 by @SamSwaroop
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

--- src/components/student/dashboard/ExamTab.jsx
+++ src/components/student/dashboard/ExamTab.jsx
@@ -1,166 +1,291 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
+// import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import questions from "../../../upload.json"
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import Button from "@material-ui/core/Button";
+import questions from "../../../upload.json";
 
-import * as XLSX from "xlsx";
+import { Grid, Header } from "semantic-ui-react";
+import { Component } from "react";
 
-import { Grid, Header, List } from "semantic-ui-react";
+// const useStyles = withStyles(() => ({
+//   root: {
+//     minWidth: 275,
+//   },
+//   pos: {
+//     textAlign: "center",
+//     marginBottom: 12,
+//   },
+// }));
 
-const useStyles = makeStyles(() => ({
-  root: {
-    minWidth: 275,
-    
-  },
-  pos: {
-    
-    textAlign: "center",
-    marginBottom: 12,
-  },
-}));
+// export default function ExamTab() {
+//   const [value, setValue] = React.useState("");
+//   const [state, setState] = React.useState([{
+//     Index: 0,
+//   }]);
 
+//   const handleNext = (event) => {
+//     event.preventDefault();
 
+//     if (value === "best") {
+//     }
+//   };
 
-export default function ExamTab() {
+//    const handleRadioChange = (event) => {
+//      setValue(event.target.value);
+//    };
 
-  const [items, setItems] = useState([]);
+//   const classes = useStyles();
 
-  const readExcel = (file) => {
-    const promise = new Promise((resolve, reject) => {
-      const fileReader = new FileReader();
-      fileReader.readAsArrayBuffer(file);
+//   return (
+//     <div>
+//       <Card
+//         variant="outlined"
+//         style={{
+//           width: 1000,
+//           marginTop: 100,
+//           marginLeft: 190,
+//         }}
+//       >
+//         <CardContent>
+//           <Typography variant="body2" component="p" className={classes.pos}>
+//             <h1>Welcome To Exam Application </h1>
+//             <br />
+//             <br />
+//           </Typography>
+//           <Typography variant="body1" component="p">
+//             <b>Answer the following choose the correct answers </b>
+//           </Typography>
+//           <br />
+//         </CardContent>
+//       </Card>
 
-      fileReader.onload = (e) => {
-        const bufferArray = e.target.result;
+//       <Card
+//         variant="outlined"
+//         style={{
+//           width: 1000,
+//           marginTop: 20,
+//           marginLeft: 190,
+//         }}
+//       >
+//         <CardContent>
+//           <Grid>
+//             <Grid.Row>
+//               <Grid.Column>
 
-        const wb = XLSX.read(bufferArray, { type: "buffer" });
+//                 <form onSubmit={handleNext}>
+//                   <FormControl
+//                     component="fieldset"
+//                     className={classes.formControl}
+//                   >
+//                     <FormLabel component="legend">{questions[state.Index].Question}</FormLabel>
+//                     <RadioGroup
+//                       aria-label="quiz"
+//                       name="quiz"
+//                       value={value}
+//                       onChange={handleRadioChange}
+//                     >
+//                       <FormControlLabel
+//                         value="A"
+//                         control={<Radio />}
+//                         label="A"
+//                       />
+//                       <FormControlLabel
+//                         value="B"
+//                         control={<Radio />}
+//                         label="B"
+//                       />
+//                     </RadioGroup>
 
-        const wsname = wb.SheetNames[0];
+//                     <Button
+//                       type="submit"
+//                       variant="outlined"
+//                       color="primary"
+//                       className={classes.button}
+//                     >
+//                       Next
+//                     </Button>
+//                   </FormControl>
+//                 </form>
 
-        const ws = wb.Sheets[wsname];
+//                 {/* {questions.map((el) => {
+//                   return (
+//                     <List.Item key={el.Question}>
+//                       <br></br>
+//                       <List.Content>{el.Question}</List.Content>
+//                       <List.Content>
+//                         {el.A !== undefined && (
+//                           <>
+//                             <input type="radio" name="option" value="A" />
+//                             <label for="A">{el.A}</label>
+//                           </>
+//                         )}
+//                       </List.Content>
+//                       <List.Content>
+//                         {el.B !== undefined && (
+//                           <>
+//                             <input type="radio" name="option" value="B" />
+//                             <label for="B">{el.B}</label>
+//                             <br></br>
+//                           </>
+//                         )}
+//                       </List.Content>
+//                       <List.Content>
+//                         {el.C !== undefined && (
+//                           <>
+//                             <input type="radio" name="option" value="C" />
+//                             <label for="C">{el.C}</label>
+//                             <br></br>
+//                           </>
+//                         )}
+//                       </List.Content>
+//                       <List.Content>
+//                         {el.D !== undefined && (
+//                           <>
+//                             <input type="radio" name="option" value="D" />
+//                             <label for="D">{el.D}</label>
+//                             <br></br>
+//                           </>
+//                         )}
+//                       </List.Content>
+//                       <List.Content>
+//                         {el.E !== undefined && (
+//                           <>
+//                             <input type="radio" name="option" value="E" />
+//                             <label for="E">{el.E}</label>
+//                             <br></br>
+//                           </>
+//                         )}
+//                       </List.Content>
+//                     </List.Item>
+//                   );
+//                 })} */}
+//               </Grid.Column>
+//             </Grid.Row>
+//           </Grid>
+//         </CardContent>
+//       </Card>
+//     </div>
+//   );
+// }
 
-        const data = XLSX.utils.sheet_to_json(ws);
+class ExamTab extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      index: 0,
+    };
+  }
 
-        resolve(data);
-      };
+  handleNext = (event) => {
+    event.preventDefault();
+  };
 
-      fileReader.onerror = (error) => {
-        reject(error);
-      };
-    });
+  handleRadioChange = (event) => {
+    this.setState(event.target.value);
+  };
 
-    promise.then((d) => {
-      setItems(d);
+  handleClick = () => {
+    this.setState({
+      index: this.state.index + 1,
     });
   };
-  // const history = useHistory();
 
-  // const routeChange = () => {
-  //   let path = "/login";
-  //   history.push(path);
-  // };
+  render() {
+    // const { classes } = this.props;
+    if (questions[(questions.length) - 1] === questions[this.state.index]) {
+      return (
+        <div> <h3>End of Quiz</h3> <button> Submit</button> </div >
+      )
+    }
+    else {
+      return (
+        <div>
+          <Card
+            variant="outlined"
+            style={{
+              width: 1000,
+              marginTop: 100,
+              marginLeft: 190,
+            }}
+          >
+            <CardContent>
+              <Typography variant="body2" component="p">
+                <h1>Welcome To Exam Application </h1>
+                <br />
+                <br />
+              </Typography>
+              <Typography variant="body1" component="p">
+                <b>Answer the following choose the correct answers </b>
+              </Typography>
+              <br />
+            </CardContent>
+          </Card>
 
-  const classes = useStyles();
+          <Card
+            variant="outlined"
+            style={{
+              width: 1000,
+              marginTop: 20,
+              marginLeft: 190,
+            }}
+          >
+            <CardContent>
+              <Grid>
+                <Grid.Row>
+                  <Grid.Column>
+                    <form onSubmit={this.handleNext}>
+                      <FormControl component="fieldset">
+                        <FormLabel component="legend">
+                          {questions[this.state.index].Question}
+                        </FormLabel>
+                        <RadioGroup
+                          aria-label="quiz"
+                          name="quiz"
+                          value={this.value}
+                          onChange={this.handleRadioChange}
+                        >
+                          <FormControlLabel
+                            value={questions[this.state.index].A}
+                            control={<Radio />}
+                            label={questions[this.state.index].A}
+                          />
+                          <FormControlLabel
+                            value={questions[this.state.index].B}
+                            control={<Radio />}
+                            label={questions[this.state.index].B}
+                          />
+                        </RadioGroup>
 
-  return (
-    <div>
-      <Card
-        variant="outlined"
-        style={{
-          width: 1000,
-          marginTop: 100,
-          marginLeft: 190,
-        }}
-      >
-        <CardContent>
-          <Typography variant="body2" component="p" className={classes.pos}>
-            <h1>Welcome To Exam Application </h1>
-            <br />
-            <br />
-          </Typography>
-          <Typography variant="body1" component="p">
-            <b>Answer the following choose the correct answers </b>
-          </Typography>
-          <br />
-
-          
-
-        </CardContent>
-      </Card>
-
-      <Card
-        variant="outlined"
-        style={{
-          width: 1000,
-          marginTop: 20,
-          marginLeft: 190,
-        }}
-      >
-        <CardContent>
-          <Grid>
-            <Grid.Row>
-              <Grid.Column>
-                <Header>Quiz</Header>
-                <List>
-                  {questions.map((el) => {
-                    return (
-                      <List.Item key={el.Question}>
-                        <br></br>
-                        <List.Content>
-                          {el.Question}
-                        </List.Content>
-                        <List.Content>
-                        { el.A!==undefined &&(
-                          <>
-                            <input type="radio" id="male" name="gender" value="male" />
-                            <label for="male">{el.A}</label>
-                          </>
-                        )}
-                        </List.Content>
-                        <List.Content>
-                        { el.B!==undefined &&(
-                          <>
-                            <input type="radio" id="male" name="gender" value="male" />
-                            <label for="male">{el.B}</label><br></br>
-                          </>
-                        )}
-                        </List.Content>
-                        <List.Content>
-                        { el.C!==undefined &&(
-                          <>
-                            <input type="radio" id="male" name="gender" value="male" />
-                            <label for="male">{el.C}</label><br></br>
-                          </>
-                        )}
-                        </List.Content>
-                        <List.Content>
-                        { el.D!==undefined &&(
-                          <>
-                            <input type="radio" id="male" name="gender" value="male" />
-                            <label for="male">{el.D}</label><br></br>
-                          </>
-                        )}
-                        </List.Content>
-                        <List.Content>
-                        { el.E!==undefined &&(
-                          <>
-                            <input type="radio" id="male" name="gender" value="male" />
-                            <label for="male">{el.E}</label><br></br>
-                          </>
-                        )}  
-                        </List.Content>
-
-                      </List.Item>
-                    );
-                  })}
-                </List>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </CardContent>
-      </Card>
-    </div>
-  );
+                        <Button
+                          type="submit"
+                          variant="outlined"
+                          color="primary"
+                          onClick={this.handleClick}
+                        >
+                          Next
+                        </Button>
+                      </FormControl>
+                    </form>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </CardContent>
+          </Card>
+        </div>
+      );
+    }
+  }
 }
+export default ExamTab;
+
+
+
+
