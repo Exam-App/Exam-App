d90a435c125634efb895618d3f39cb5d9e803ea3
vamsee9 <4n0n5ur7@protonmail.com>
2021-06-02 18:26:56 +0530

    code improvement
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

--- router/filesRoute.js
+++ router/filesRoute.js
@@ -1,28 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const fs = require('fs');
+const fs = require("fs");
 
+router.post("/sendFile", (request) => {
+  try {
+    fs.writeFileSync("upload.json", JSON.stringify(request.body, null, 2));
+  } catch (err) {
+    console.error(err);
+  }
 
-
-
-
-
-router.post('/sendfile',(request)=>{
-    // let {Question,A,B,C,D,E,Answer}=request.body;
-    // console.log(request.body)
-    // var data=JSON.stringify(request.data)
-    
-       
-    try {
-      fs.writeFileSync('upload.json', JSON.stringify(request.body,null,2))
-    } catch (err) {
-      console.error(err)
-    }
-   
-  
-  
-  
   // console.log(request.body)
-    
-  })
+});
 module.exports = router;
