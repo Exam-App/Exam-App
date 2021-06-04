941e030e944274b959a53d979e82e23d038cad09
vamsee9 <4n0n5ur7@protonmail.com>
2021-06-04 18:25:30 +0530

    timer UI adjustments
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

--- src/components/student/dashboard/ExamTab.jsx
+++ src/components/student/dashboard/ExamTab.jsx
@@ -1,35 +1,38 @@
 import React, { Component } from "react";
 import questions from "../../../upload.json";
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import Box from '@material-ui/core/Box';
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
 import Timer from "react-compound-timer";
+import { withStyles } from "@material-ui/core";
 
-
-
-const useStyles = makeStyles({
+const styles = (theme) => ({
   root: {
     minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
+    // backgroundColor: "#151515",
   },
   pos: {
-    marginBottom: 12,
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  cardStyle: {
+    width: 1000,
+    marginTop: 15,
+    marginBottom: 15,
+    marginLeft: 250,
+  },
+  timeStyle: {
+    width: 300,
+    marginTop: 80,
+    marginLeft: 950,
   },
 });
 
 class ExamTab extends Component {
-  
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       index: 0,
       score: 0,
@@ -60,291 +63,172 @@ class ExamTab extends Component {
 
   render() {
 
-    return (
-      <>
+    const Timeout = () => {
+      return (
         <div>
-        <br></br>
-              <br></br>
-              <br></br><br></br>
-              <br></br>
-              <br></br>
-              <Timer
-                initialTime={60000 * 60 * 2}
-                lastUnit="h"
-                direction="backward"
-              >
-              {() => (
-                <>
-                  <button>
-                  <Timer.Hours /> :
-                  
-                  <Timer.Minutes /> :
-                  <Timer.Seconds /> 
-                  </button>
-                </>
-              )}
-              </Timer>
-        <Card style={{ marginLeft:200, marginRight:200}} >
-      <CardContent style={{ marginLeft:30}}>
-        <Typography variant="h4" component="h2" align="center">
-        <Box fontWeight="fontWeightBold" fontStyle="italic" m={1}>
-        Quiz
-      </Box>
-        </Typography>
-      <Typography variant="h6" component="h2">
-        
-        
-          {questions[this.state.index]=== undefined ? (
-            
-            <div>    
-            
-              
-            <Typography variant="h4" component="h2" align="center">
-            <Box fontWeight="fontWeightBold" fontStyle="italic" m={1}>
-            End of the Quiz
-          </Box>
-          </Typography>
-              <br></br>
-              
-            <h1 align="center">The score is:{this.state.score}</h1>
-          </div>
-          ) : (
-            <form onSubmit={this.formSubmit}>
-          <div className="radio">
-            <br></br>
-            <br></br>
-            <br></br>
-            <p>{questions[this.state.index].Question}</p>
-            <label>
-            {questions[this.state.index].A !== undefined &&(
-              <input
-                type="radio"
-                value={questions[this.state.index].A}
-                checked={
-                  this.state.selectedOption === questions[this.state.index].A
-                }
-                onChange={this.onValueChange}
-              />
-            )}
-              {questions[this.state.index].A}
-            </label>
-          </div>
-          <div className="radio">
-            <label>
-            {questions[this.state.index].B !== undefined &&(
-              <input
-                type="radio"
-                value={questions[this.state.index].B}
-                checked={
-                  this.state.selectedOption === questions[this.state.index].B
-                }
-                onChange={this.onValueChange}
-              />
-            )}
-              {questions[this.state.index].B}
-            </label>
-          </div>
-          <div className="radio">
-            <label>
-            {questions[this.state.index].C !== undefined &&(
-              <input
-                type="radio"
-                value={questions[this.state.index].C}
-                checked={
-                  this.state.selectedOption === questions[this.state.index].C
-                }
-                onChange={this.onValueChange}
-              />
-            )}
-              {questions[this.state.index].C}
-            </label>
-          </div>
-          <div className="radio">
-            <label>
-            {questions[this.state.index].D !== undefined &&(
-              <input
-                type="radio"
-                value={questions[this.state.index].D}
-                checked={
-                  this.state.selectedOption === questions[this.state.index].D
-                }
-                onChange={this.onValueChange}
-              />
-            )}
-              {questions[this.state.index].D}
-            </label>
-          </div>
-          <div className="radio">
-            <label>
-            {questions[this.state.index].E !== undefined &&(
-              <input
-                type="radio"
-                value={questions[this.state.index].E}
-                checked={
-                  this.state.selectedOption === questions[this.state.index].E
-                }
-                onChange={this.onValueChange}
-              />
-            )}
-              {questions[this.state.index].E}
-            </label>
-          </div>
-          <div>Selected option is : {this.state.selectedOption}</div>
-          <button  variant="outlined" type="submit" >
-          <Typography variant="h6" style={{ color: "#7e57c2" }} p={50}>
-                    Next Question
-              </Typography>
-          </button>
           <br />
-          <h1 align="center">{this.state.score}</h1>
-        </form>
-          )}
+          <br />
+          <br />
+          <br />
+          <Typography variant="h4" component="h2" align="center">
+            <Box fontWeight="fontWeightBold" fontStyle="italic" m={1}>
+              End of the Quiz
+            </Box>
           </Typography>
+          <br></br>
+        </div>
+      );
+    };
+    return (
+      <div>
+        <Card variant="outlined" className={this.props.classes.timeStyle}>
+          <Timer
+            initialTime={10000 * 1 * 1}
+            lastUnit="h"
+            direction="backward"
+            checkpoints={[
+              {
+                time: 0,
+                callback: () => Timeout(),
+              },
+            ]}
+          >
+            <Typography align="center" variant="h3">
+              <Timer.Hours /> : <Timer.Minutes /> : <Timer.Seconds />
+            </Typography>
+          </Timer>
+        </Card>
+
+        <Card variant="outlined" className={this.props.classes.cardStyle}>
+          <CardContent style={{ marginLeft: 30 }}>
+            <Typography variant="h4" component="h2" align="center" gutterBottom>
+              <Box fontWeight="fontWeightBold" m={1}>
+                Chooses
+              </Box>
+            </Typography>
+            <Typography variant="h6" component="h2">
+              {questions[this.state.index] === undefined ? (
+                <div>
+                  <Typography variant="h4" align="center">
+                    <Box fontWeight="fontWeightBold">End of the Quiz</Box>
+                  </Typography>
+
+                  <Typography variant="h4" align="center">
+                    your score is: {this.state.score} / {questions.length}
+                  </Typography>
+                </div>
+              ) : (
+                <form onSubmit={this.formSubmit}>
+                  <div className="radio">
+                    <br></br>
+
+                    <p>{questions[this.state.index].Question}</p>
+                    <label>
+                      {questions[this.state.index].A !== undefined && (
+                        <input
+                          type="radio"
+                          value={questions[this.state.index].A}
+                          checked={
+                            this.state.selectedOption ===
+                            questions[this.state.index].A
+                          }
+                          onChange={this.onValueChange}
+                        />
+                      )}
+                      {questions[this.state.index].A}
+                    </label>
+                  </div>
+                  <div className="radio">
+                    <label>
+                      {questions[this.state.index].B !== undefined && (
+                        <input
+                          type="radio"
+                          value={questions[this.state.index].B}
+                          checked={
+                            this.state.selectedOption ===
+                            questions[this.state.index].B
+                          }
+                          onChange={this.onValueChange}
+                        />
+                      )}
+                      {questions[this.state.index].B}
+                    </label>
+                  </div>
+                  <div className="radio">
+                    <label>
+                      {questions[this.state.index].C !== undefined && (
+                        <input
+                          type="radio"
+                          value={questions[this.state.index].C}
+                          checked={
+                            this.state.selectedOption ===
+                            questions[this.state.index].C
+                          }
+                          onChange={this.onValueChange}
+                        />
+                      )}
+                      {questions[this.state.index].C}
+                    </label>
+                  </div>
+                  <div className="radio">
+                    <label>
+                      {questions[this.state.index].D !== undefined && (
+                        <input
+                          type="radio"
+                          value={questions[this.state.index].D}
+                          checked={
+                            this.state.selectedOption ===
+                            questions[this.state.index].D
+                          }
+                          onChange={this.onValueChange}
+                        />
+                      )}
+                      {questions[this.state.index].D}
+                    </label>
+                  </div>
+                  <div className="radio">
+                    <label>
+                      {questions[this.state.index].E !== undefined && (
+                        <input
+                          type="radio"
+                          value={questions[this.state.index].E}
+                          checked={
+                            this.state.selectedOption ===
+                            questions[this.state.index].E
+                          }
+                          onChange={this.onValueChange}
+                        />
+                      )}
+                      {questions[this.state.index].E}
+                    </label>
+                  </div>
+                  {/* <div>Selected option is : {this.state.selectedOption}</div> */}
+                  <Button
+                    variant="outlined"
+                    type="submit"
+                    style={{
+                      marginLeft: 680,
+                      marginTop: 15,
+                    }}
+                  >
+                    <Typography
+                      variant="h6"
+                      style={{ color: "#7e57c2" }}
+                      p={50}
+                    >
+                      Next Question
+                    </Typography>
+                  </Button>
+                  <br />
+                  {/* <h1 align="center">{this.state.score}</h1> */}
+                </form>
+              )}
+            </Typography>
           </CardContent>
         </Card>
-        </div>
-
-      
-
-        
-      </>
-
+      </div>
     );
   }
 }
 
-export default ExamTab;
-//   return (
-//     <div>
-//       <Card
-//         variant="outlined"
-//         style={{
-//           width: 1000,
-//           marginTop: 100,
-//           marginLeft: 190,
-//         }}
-//       >
-//         <CardContent>
-//           <Typography variant="body2" component="p" className={classes.pos}>
-//             <h1>Welcome To Exam Application </h1>
-//             <br />
-//             <br />
-//           </Typography>
-//           <Typography variant="body1" component="p">
-//             <b>Answer the following choose the correct answers </b>
-//           </Typography>
-//           <br />
-//         </CardContent>
-//       </Card>
-
-//       <Card
-//         variant="outlined"
-//         style={{
-//           width: 1000,
-//           marginTop: 20,
-//           marginLeft: 190,
-//         }}
-//       >
-//         <CardContent>
-//           <Grid>
-//             <Grid.Row>
-//               <Grid.Column>
-
-//                 <form onSubmit={handleNext}>
-//                   <FormControl
-//                     component="fieldset"
-//                     className={classes.formControl}
-//                   >
-//                     <FormLabel component="legend">{questions[state.Index].Question}</FormLabel>
-//                     <RadioGroup
-//                       aria-label="quiz"
-//                       name="quiz"
-//                       value={value}
-//                       onChange={handleRadioChange}
-//                     >
-//                       <FormControlLabel
-//                         value="A"
-//                         control={<checkbox />}
-//                         label="A"
-//                       />
-//                       <FormControlLabel
-//                         value="B"
-//                         control={<Radio />}
-//                         label="B"
-//                       />
-//                     </RadioGroup>
-
-//                     <Button
-//                       type="submit"
-//                       variant="outlined"
-//                       color="primary"
-//                       className={classes.button}
-//                     >
-//                       Next
-//                     </Button>
-//                   </FormControl>
-//                 </form>
-
-//                 {/* {questions.map((el) => {
-//                   return (
-//                     <List.Item key={el.Question}>
-//                       <br></br>
-//                       <List.Content>{el.Question}</List.Content>
-//                       <List.Content>
-//                         {el.A !== undefined && (
-//                           <>
-//                             <input type="radio" name="option" value="A" />
-//                             <label for="A">{el.A}</label>
-//                           </>
-//                         )}
-//                       </List.Content>
-//                       <List.Content>
-//                         {el.B !== undefined && (
-//                           <>
-//                             <input type="radio" name="option" value="B" />
-//                             <label for="B">{el.B}</label>
-//                             <br></br>
-//                           </>
-//                         )}
-//                       </List.Content>
-//                       <List.Content>
-//                         {el.C !== undefined && (
-//                           <>
-//                             <input type="radio" name="option" value="C" />
-//                             <label for="C">{el.C}</label>
-//                             <br></br>
-//                           </>
-//                         )}
-//                       </List.Content>
-//                       <List.Content>
-//                         {el.D !== undefined && (
-//                           <>
-//                             <input type="radio" name="option" value="D" />
-//                             <label for="D">{el.D}</label>
-//                             <br></br>
-//                           </>
-//                         )}
-//                       </List.Content>
-//                       <List.Content>
-//                         {el.E !== undefined && (
-//                           <>
-//                             <input type="radio" name="option" value="E" />
-//                             <label for="E">{el.E}</label>
-//                             <br></br>
-//                           </>
-//                         )}
-//                       </List.Content>
-//                     </List.Item>
-//                   );
-//                 })} */}
-//               </Grid.Column>
-//             </Grid.Row>
-//           </Grid>
-//         </CardContent>
-//       </Card>
-//     </div>
-//   );
-//     }
-//   }
-// }
-// export default ExamTab;
+export default withStyles(styles)(ExamTab);
